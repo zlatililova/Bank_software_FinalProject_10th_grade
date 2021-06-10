01c3177be0ca00e4b91683410eb29405cf66cb15
Zlatina Lilova <48581738+zlatililova@users.noreply.github.com>
2021-06-10 22:37:24 +0300

    Update register.c

--- register.c
+++ register.c
@@ -4,7 +4,7 @@
 
 struct user_t{
 
-    char username[21];
+    char username[11];
     int hash_pass;
     unsigned int id;
 
@@ -17,67 +17,113 @@ struct account_t{
     unsigned int id;
 };
 
-void register_user (void){
-    struct user_t new_user;
-    char password[11];
-
-    printf("Enter a user name between 5 and 20 characters:\n");
-    fgets(new_user.username, 20, stdin);
-
-    while(strlen(new_user.username)<6){
-         printf("Invalid data. Enter a user name between 5 and 20 characters:\n");
-        fgets(new_user.username, 20, stdin);
-    }
-
-    printf("Enter a password between 6 and 10 characters:\n");
-    fgets(password, 10, stdin);
-    while(strlen(password)<7){
-         printf("Invalid data. Enter a password between 6 and 10 characters:\n");
-        fgets(password, 10, stdin);
-    }
-
-    new_user.hash_pass = hashing(password);
-
-
-}
-void return_last_id(int i){
-    FILE *fptr;
-    int last_id;
-
-   if ((fptr = fopen("C:\\Users\\user\\Desktop\\text.txt","r+")) == NULL){
-       printf("Error! opening file");
-
-       // Program exits if the file pointer returns NULL.
-       exit(1);
-   }
-
-   // Moves the cursor to the end of the file
-   fseek(fptr, -sizeof(int), SEEK_END);
-    printf("last id = %d\n",last_id);
-
-      fread(&last_id, sizeof(int), 1, fptr);
-      //printf("otvorih se\n");
-      printf("n1: %d\n", last_id);
-
-      fclose(fptr);
-
-
-
-}
-
 int hashing (char password[]){
     int pass_len = strlen(password);
-    printf("trlen = %d\n",pass_len);
     int sum = 0;
 
     for(int i = 0; i<pass_len-1; i++){
         sum += password[i];
     }
-    return sum ;
+    return sum;
+}
+
+int validate(char name[]){
+int l = strlen(name);
+int flag = 1;
+    //check length is 0 or more than 30
+    if (l < 6 || l > 11)
+        return 0;
+
+    for (int i = 0; name[i]!='\0'; i++)
+    {
+        char s = name[i];
+
+        if (s>='a'&&s<='z'){
+            flag = 1;
+        }
+        else if(s>='0'&& s<='9'){
+            flag = 1;
+        }
+        else if(s == '\n'){
+            flag = 1;
+        }
+        else{
+          return 0;
+        }
+    }
+    return flag;
+}
+
+int return_last_id(){
+    FILE *fptr;
+    int last_id;
+    char str[50];
+    char *ptr;
+
+   if ((fptr = fopen("C:\\Users\\user\\Desktop\\text.txt","a+")) == NULL){
+       printf("Error! opening file");
+       exit(1);
+   }
+    while (!feof(fptr)){
+        fgets(str, sizeof(str), fptr);
+    }
+    last_id = (int)strtol(str, &ptr, 10);
+    fclose(fptr);
+
+    return last_id;
+}
+
+struct user_t register_user(void){
+    struct user_t new_user;
+    char password[11];
+    char name[11];
+    int is_valid = 0;
+
+    printf("Enter a user name between 5 and 10 characters, use only letters and numbers:\n");
+    fgets(name, 10, stdin);
+    is_valid = validate(name);
+
+    while(is_valid!=1){
+        printf("Invalid data. Enter a user name between 5 and 20 characters, use only letters and numbers:\n");
+        fgets(name, 10, stdin);
+        is_valid = validate(name);
+    }
+
+    printf("Enter a password between 5 and 10 characters, use only letters and numbers:\n");
+    fgets(password, 10, stdin);
+    is_valid = validate(password);
+
+    while(is_valid!=1){
+        printf("Invalid data. Enter a password between 5 and 10 characters, use only letters and numbers:\n");
+        fgets(name, 10, stdin);
+        is_valid = validate(name);
+    }
+
+
+        name[strlen(name)-1]='\0';
+
+
+    strcpy(new_user.username, name);
+    new_user.hash_pass = hashing(password);
+    new_user.id = return_last_id();
+    new_user.id+=1;
+
+    return new_user;
+}
+
+void append_user(struct user_t *user){
+    FILE *fp;
+    fp = fopen("C:\\Users\\user\\Desktop\\text.txt", "a+");
+    fprintf(fp, "%d %s %d %c", user->id, user->username, user->hash_pass, '\n');
+
+    fclose(fp);
+
 }
 
 int main(){
 
-    register_user();
+    struct user_t u = register_user();
+    append_user(&u);
+
     return 0;
 }
